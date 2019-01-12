e50ecab9b73becbb9e29c2d741df361124b8c8ac
Ian <ian.mcvann@gmail.com>
2019-01-11 19:11:36 -0800

    Added support for 2 contours

--- GreenVision_Live.cpp
+++ GreenVision_Live.cpp
@@ -1,45 +1,51 @@
 // OpenCV.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include "opencv2/imgcodecs.hpp"
 using namespace std;
 using namespace cv;
-
 int main()
 {
 	VideoCapture cap(0);
+	Mat image;
+	Mat mask;
+	Mat contourimg;
+	vector<Vec4i> hier;
+	Rect large;
+	Rect secondLarge;
+	int largestIndex = 0;
+	int largestContour = 0;
+	int secondLargestIndex = 0;
+	int secondLargestContour = 0;
+	vector<vector<Point>> Contours;
 	while (true) {
-		Mat image1;
-		Mat image2;
-		cap >> image1;
-		cap >> image2;
-		Mat finalimage;
-		Mat mask;
-		Mat contourimg = image1.clone();
-		vector<vector<Point>> Contours;
-		vector<Vec4i> hier;
-		double maxArea = 0.0;
-		int savedContour = -1;
-		cvtColor(image1, image1, COLOR_BGR2GRAY);
-		cvtColor(image2, image2, COLOR_BGR2GRAY);
-		GaussianBlur(image1, image1, Size(5, 5), 0);
-		GaussianBlur(image2, image2, Size(5, 5), 0);
-		absdiff(image1, image2, finalimage);
-		//threshold(finalimage, mask, 40, 255, THRESH_BINARY);
-		inRange(finalimage, Scalar(50.0, 55.03597122302158, 174.28057553956833), Scalar(90.60606060606061, 255, 255), mask);
+		cap >> image;
+		contourimg = image.clone();
+		cvtColor(image, image, COLOR_BGR2GRAY);
+		GaussianBlur(image, image, Size(5, 5), 0);
+		inRange(image, Scalar(45, 20, 205), Scalar(255, 255, 255), mask);
 		erode(mask, mask, 1);
 		findContours(mask, Contours, hier, RETR_TREE, CHAIN_APPROX_SIMPLE);
 		//Might need to loop through here to check for size
-		for (int i = 0; i < Contours.size(); i++) {
-			double area = contourArea(Contours[i]);
-			if (area > maxArea) {
-				maxArea = area;
-				savedContour = i;
+		for (int i = 0; i < Contours.size(); i++)
+		{
+			if (Contours[i].size() > largestContour) {
+				secondLargestContour = largestContour;
+				secondLargestIndex = largestIndex;
+				largestContour = Contours[i].size();
+				largestIndex = i;
+			}
+			else if (Contours[i].size() > secondLargestContour) {
+				secondLargestContour = Contours[i].size();
+				secondLargestIndex = i;
 			}
 		}
-		drawContours(contourimg, Contours, savedContour, Scalar(70, 237, 54), 3, 8, hier);
+		large = boundingRect(Contours[largestIndex]);
+		secondLarge = boundingRect(Contours[secondLargestIndex]);
+		drawContours(contourimg, Contours, largestIndex, Scalar(232, 12, 122), 3, 8, hier);
+		drawContours(contourimg, Contours, secondLargestIndex, Scalar(232, 12, 122), 3, 8, hier);
+
 		namedWindow("Image", WINDOW_AUTOSIZE);
 		imshow("Image", contourimg);
 		waitKey(15);
