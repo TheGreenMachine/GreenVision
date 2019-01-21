76e03d844612a7079f3884c51c74249eb064228c
ethansky <fragglekids@gmail.com>
2019-01-21 11:45:32 -0600

    Code clean up by using a NetTable replace function
    Former-commit-id: 5c2478935c03cfd80b9932fa99aeb19d61e4ee15

--- GreenVision_Live.py
+++ GreenVision_Live.py
@@ -22,26 +22,47 @@ elif len(sys.argv) == 3:
     visionFlag = sys.argv[1] == "-v" or sys.argv[2] == "-v"
     debugFlag = sys.argv[1] == "-d" or sys.argv[2] == "-d"
 if debugFlag:
-    print("Vision flag is:",visionFlag,"debug flag:",debugFlag)
+    print("Vision flag is:", visionFlag, "debug flag:", debugFlag)
 
 lower_color = np.array([50.0, 55.03597122302158, 174.28057553956833])
 upper_color = np.array([90.60606060606061, 255, 255])
-def defineRecs(rectangle):
+
+
+def defineRec(rectangle):
     topleftX = rectangle[0]
     topleftY = rectangle[1]
     width = rectangle[2]
-    height= rectangle[3]
+    height = rectangle[3]
     bottomrightX = topleftX + width
     bottomrightY = topleftY + height
     centerX = int((topleftX + bottomrightX) / 2)
     centerY = int((topleftY + bottomrightY) / 2)
-    
+
     return topleftX, topleftY, bottomrightX, bottomrightY, centerX, centerY
+
+
 def getAverage(center1x, center2x, center1y, center2y):
     averagedCenterX = int((center1x + center2x) / 2)
     averagedCenterY = int((center1y + center2y) / 2)
 
     return averagedCenterX, averagedCenterY
+
+def updateNetTable(n, center1x = -1, center1y = -1, center2x = -1, center2y = -1, averagedCenterX = -1, averagedCenterY = -1, debugFlag = False):
+        table.putNumber("center{n}X".format(n=n), center1x)
+        table.putNumber("center{n}Y".format(n=n), center1y)
+        table.putNumber("center{n}X".format(n=n+1), center2x)
+        table.putNumber("center{n}Y".format(n=n+1), center2y)
+        table.putNumber("averagedCenterX", averagedCenterX)
+        table.putNumber("averagedCenterY", averagedCenterY)
+        if debugFlag:
+            print("center{n}X".format(n=n), center1x)
+            print("center{n}Y".format(n=n), center1y)
+            print("center{n}X".format(n=n + 1), center2x)
+            print("center{n}Y".format(n=n + 1), center2y)
+            print("averagedCenterX", averagedCenterX)
+            print("averagedCenterY", averagedCenterY)
+
+
 while True:
     _, frame = cap.read()
 
@@ -54,49 +75,40 @@ while True:
     ncontours = []
     for contour in contours:
         if cv2.contourArea(contour) > 75:
-            print('Contour area:',cv2.contourArea(contour))
+            print('Contour area:', cv2.contourArea(contour))
             ncontours.append(contour)
     print("Number of contours: ", len(ncontours))
     rectangles = []
     for c in ncontours:
         cv2.drawContours(frame, [c], -1, (0, 0, 255), 3)
         rectangles.append(cv2.boundingRect(c))
-    if (len(rectangles)) > 0:
-        
-        topLeft1x, topLeft1y, bottomRight1x, bottomRight1y, center1x, center1y = defineRecs(rectangles[0])
-        if (len(rectangles)) > 1:
-            
-            topLeft2x, topLeft2y, bottomRight2x, bottomRight2y, center2x, center2y = defineRecs(rectangles[1])
+    if len(rectangles) > 0:
+
+        topLeft1x, topLeft1y, bottomRight1x, bottomRight1y, center1x, center1y = defineRec(rectangles[0])
+        if len(rectangles) > 1:
+
+            topLeft2x, topLeft2y, bottomRight2x, bottomRight2y, center2x, center2y = defineRec(rectangles[1])
             averagedCenterX, averagedCenterY = getAverage(center1x, center2x, center1y, center2y)
-            
-            cv2.line(frame, (center1x, center1y), (center1x, center1y), (255, 0, 0), 8)
-            cv2.line(frame, (center2x, center2y), (center2x, center2y), (255, 0, 0), 8)
-            cv2.line(frame, (averagedCenterX, averagedCenterY), (averagedCenterX, averagedCenterY), (255, 0, 0), 8)
-            if len(ncontours) == 0:
-                table.putNumber("center1X", -1)
-                table.putNumber("center1Y", -1)
-                table.putNumber("center2X", -1)
-                table.putNumber("center2Y", -1)
-                table.putNumber("averagedCenterX", -1)
-                table.putNumber("averagedCenterY", -1)
-                if debugFlag:
-                    print("TARGET NOT FOUND")
-            else:
-                table.putNumber("center1X", center1x)
-                table.putNumber("center1Y", center1y)
-                table.putNumber("center2X", center2x)
-                table.putNumber("center2Y", center2y)
-                table.putNumber("averagedCenterX", averagedCenterX)
-                table.putNumber("averagedCenterY", averagedCenterY)
-                if debugFlag:
-                    print("center1X", center1x)
-                    print("center1Y", center1y)
-                    print("center2X", center2x)
-                    print("center2Y", center2y)
-                    print("averagedCenterX", averagedCenterX)
-                    print("averagedCenterY", averagedCenterY)
+            updateNetTable(1, center1x, center1y, center2x, center2y, averagedCenterX, averagedCenterY)
+
+            if len(rectangles) > 2:
+                topLeft3x, topLeft3y, bottomRight3x, bottomRight3y, center3x, center3y = defineRec(rectangles[2])
+                if len(rectangles) > 3:
+                    topLeft4x, topLeft4y, bottomRight4x, bottomRight4y, center4x, center4y = defineRec(rectangles[3])
+                    averagedCenter1X, averagedCenter1Y = getAverage(center3x, center4x, center3y, center4y)
+                    updateNetTable(2, center3x, center3y, center4x, center4y, averagedCenter1X, averagedCenter1Y)
+                    if len(rectangles) > 4:
+                        topLeft5x, topLeft5y, bottomRight5x, bottomRight5y, center5x, center5y = defineRec(rectangles[4])
+                        if len(rectangles) > 5:
+                            topLeft6x, topLeft6y, bottomRight6x, bottomRight6y, center6x, center6y = defineRec(rectangles[5])
+                            averagedCenter2X, averagedCenter2Y = getAverage(center5x, center6x, center5y, center6y)
+                            updateNetTable(3, center5x, center5y, center6x, center6y, averagedCenter2X, averagedCenter2Y)
+
+                            cv2.line(frame, (center1x, center1y), (center1x, center1y), (255, 0, 0), 8)
+                            cv2.line(frame, (center2x, center2y), (center2x, center2y), (255, 0, 0), 8)
+                            cv2.line(frame, (averagedCenterX, averagedCenterY), (averagedCenterX, averagedCenterY), (255, 0, 0), 8)
+
     if visionFlag:
         cv2.imshow('Contour Window', frame)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
-
