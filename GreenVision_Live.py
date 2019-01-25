d00d8682670cce898e1ac7b830845840be531339
ethansky <fragglekids@gmail.com>
2019-01-24 20:25:21 -0600

    Refactor to follow PEP8 formatting

--- GreenVision_Live.py
+++ GreenVision_Live.py
@@ -5,7 +5,6 @@ import networktables as nt
 from imutils.video import WebcamVideoStream
 import json
 
-
 vision_flag = '-v' in sys.argv
 debug_flag = '-d' in sys.argv
 threshold_flag = '-t' in sys.argv
@@ -24,10 +23,14 @@ table.putNumber("visionX", -1)
 table.putNumber("visionY", -1)
 
 if debug_flag:
-    print('Vision flag: {v}\nDebug flag: {d}\nThreshold Flag: {t}\nMultithread Flag: {mt}'.format(v=vision_flag, d=debug_flag, t=threshold_flag, mt=multithread_flag))
+    print('Vision flag: {v}\nDebug flag: {d}\nThreshold Flag: {t}\nMultithread Flag: {mt}'.format(v=vision_flag,
+                                                                                                  d=debug_flag,
+                                                                                                  t=threshold_flag,
+                                                                                                  mt=multithread_flag))
 
 lower_color = np.array(data["lower-color-list-thresh"]) if threshold_flag else np.array(data["lower-color-list"])
 upper_color = np.array(data["upper-color-list-thresh"]) if threshold_flag else np.array(data["upper-color-list"])
+
 
 def draw_points(frame, center1x, center1y, center2x, center2y, averagedCenterX, averagedCenterY):
     cv2.line(frame, (center1x, center1y), (center1x, center1y), (255, 0, 0), 8)
@@ -58,24 +61,25 @@ def get_avg_points(center1x, center2x, center1y, center2y):
 def is_pair(topLeftX, topLeftX1, bottomRightX, bottomRightX1):
     topDiff = abs(topLeftX - topLeftX1)
     bottomDiff = abs(bottomRightX - bottomRightX1)
-    print('Top diff: {td}\nBottom diff: {bd}'.format(td=topDiff,bd=bottomDiff))
+    print('Top diff: {td}\nBottom diff: {bd}'.format(td=topDiff, bd=bottomDiff))
     return bottomDiff > topDiff
-        
-    
-def update_net_table(n, center1x = -1, center1y = -1, center2x = -1, center2y = -1, averagedCenterX = -1, averagedCenterY = -1, debug_flag = False):
-        table.putNumber("center{n}X".format(n=n), center1x)
-        table.putNumber("center{n}Y".format(n=n), center1y)
-        table.putNumber("center{n}X".format(n=n+1), center2x)
-        table.putNumber("center{n}Y".format(n=n+1), center2y)
-        table.putNumber("averagedCenterX", averagedCenterX)
-        table.putNumber("averagedCenterY", averagedCenterY)
-        if debug_flag:
-            print("center{n}X".format(n=n), center1x)
-            print("center{n}Y".format(n=n), center1y)
-            print("center{n}X".format(n=n + 1), center2x)
-            print("center{n}Y".format(n=n + 1), center2y)
-            print("averagedCenterX", averagedCenterX)
-            print("averagedCenterY", averagedCenterY)
+
+
+def update_net_table(n, center1x=-1, center1y=-1, center2x=-1, center2y=-1, averagedCenterX=-1, averagedCenterY=-1,
+                     debug_flag=False):
+    table.putNumber("center{n}X".format(n=n), center1x)
+    table.putNumber("center{n}Y".format(n=n), center1y)
+    table.putNumber("center{n}X".format(n=n + 1), center2x)
+    table.putNumber("center{n}Y".format(n=n + 1), center2y)
+    table.putNumber("averagedCenterX", averagedCenterX)
+    table.putNumber("averagedCenterY", averagedCenterY)
+    if debug_flag:
+        print("center{n}X".format(n=n), center1x)
+        print("center{n}Y".format(n=n), center1y)
+        print("center{n}X".format(n=n + 1), center2x)
+        print("center{n}Y".format(n=n + 1), center2y)
+        print("averagedCenterX", averagedCenterX)
+        print("averagedCenterY", averagedCenterY)
 
 
 while True:
@@ -103,8 +107,9 @@ while True:
             topLeft2X, topLeft2Y, bottomRight2X, bottomRight2Y, center2X, center2Y = def_rec(rectangles[1])
             averagedCenterX, averagedCenterY = get_avg_points(center1X, center2X, center1Y, center2Y)
             if True:
-            #if is_pair(topLeft1X, topLeft2X, bottomRight1X, bottomRight2X):
-                update_net_table(1, center1X, center1Y, center2X, center2Y, averagedCenterX, averagedCenterY, debug_flag)
+                # if is_pair(topLeft1X, topLeft2X, bottomRight1X, bottomRight2X):
+                update_net_table(1, center1X, center1Y, center2X, center2Y, averagedCenterX, averagedCenterY,
+                                 debug_flag)
                 draw_points(frame, center1X, center1Y, center2X, center2Y, averagedCenterX, averagedCenterY)
 
             if len(rectangles) > 3:
@@ -112,7 +117,8 @@ while True:
                 topLeft4X, topLeft4Y, bottomRight4X, bottomRight4Y, center4X, center4Y = def_rec(rectangles[3])
                 averagedCenter1X, averagedCenter1Y = get_avg_points(center3X, center4X, center3Y, center4Y)
                 if is_pair(topLeft3X, topLeft4X, bottomRight3X, bottomRight4X):
-                    update_net_table(2, center3X, center3Y, center4X, center4Y, averagedCenter1X, averagedCenter1Y, debug_flag)
+                    update_net_table(2, center3X, center3Y, center4X, center4Y, averagedCenter1X, averagedCenter1Y,
+                                     debug_flag)
                     draw_points(frame, center3X, center3Y, center4X, center4Y, averagedCenter1X, averagedCenter1Y)
 
                 if len(rectangles) > 5:
@@ -120,7 +126,8 @@ while True:
                     topLeft6X, topLeft6Y, bottomRight6X, bottomRight6Y, center6X, center6Y = def_rec(rectangles[5])
                     averagedCenter2X, averagedCenter2Y = get_avg_points(center5X, center6X, center5Y, center6Y)
                     if is_pair(topLeft5X, topLeft6X, bottomRight5X, bottomRight6X):
-                        update_net_table(3, center5X, center5Y, center6X, center6Y, averagedCenter2X, averagedCenter2Y, debug_flag)
+                        update_net_table(3, center5X, center5Y, center6X, center6Y, averagedCenter2X, averagedCenter2Y,
+                                         debug_flag)
                         draw_points(frame, center4X, center4Y, center5X, center5Y, averagedCenter2X, averagedCenter2Y)
 
     if vision_flag:
