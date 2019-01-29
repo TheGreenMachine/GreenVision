a2848bb0186879864f2a5a03475e08eaab290c5e
ethansky <fragglekids@gmail.com>
2019-01-28 20:43:06 -0600

    Add 5 second sleep
    Former-commit-id: 7c20b23dd16e7fd71e6a04283fc32ec073fa39fb

--- GreenVision_Find_VFOV.py
+++ GreenVision_Find_VFOV.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import json
 import math
+import time
 
 
 def calc_distance(p):
@@ -56,7 +57,7 @@ vertical_aspect = data['vertical-aspect']
 lower_color = np.array(data["lower-color-list"])
 upper_color = np.array(data["upper-color-list"])
 
-for v_fov in range(60, 65):
+for v_fov in range(60, 66):
     V_FOCAL_LENGTH = data['image-height'] / (2 * math.tan((v_fov / 2)))
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -71,12 +72,14 @@ for v_fov in range(60, 65):
             ncontours.append(contour)
     rec_list = []
     for c in ncontours:
+        cv2.drawContours(frame, [c], -1, (0, 0, 255), 3)
         rec_list.append(cv2.boundingRect(c))
         if len(rec_list) > 1:
             rec1 = def_rec(rec_list[0])
             rec2 = def_rec(rec_list[1])
             avg_c1_x, avg_c1_y = get_avg_points(rec1, rec2)
             if True:
+                draw_points(rec1, rec2, avg_c1_x, avg_c1_y)
                 pitch = calc_pitch(avg_c1_y, screen_c_y, V_FOCAL_LENGTH)
                 if pitch != 0:
                     distance = calc_distance(pitch)
@@ -84,6 +87,7 @@ for v_fov in range(60, 65):
                     continue
                 print('V_FOV: {}, pitch: {}, distance: {}'.format(v_fov, pitch, distance))
                 print('=========================================================')
+                time.sleep(5)
     cv2.imshow('Contour Window', frame)
     cv2.imshow('Mask', mask)
     if cv2.waitKey(1) & 0xFF == ord('q'):
