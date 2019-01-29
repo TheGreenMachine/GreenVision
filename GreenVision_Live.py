0c6ac622f11d6d5a4d2451e4d47d62f7bffaecda
ethansky <fragglekids@gmail.com>
2019-01-28 20:46:33 -0600

    Addd new center
    Former-commit-id: b1c6a4efb11755ce1870d85aa6739aa6f1adc283

--- GreenVision_Live.py
+++ GreenVision_Live.py
@@ -124,9 +124,8 @@ while True:
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_color, upper_color)
 
-    screen_h, screen_w, _ = frame.shape
-    screen_c_x = (screen_w / 2) - 0.5
-    screen_c_y = (screen_h / 2) - 0.5
+    screen_c_x = (data['image-width'] / 2) - 0.5
+    screen_c_y = (data['image-height'] / 2) - 0.5
     _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     ncontours = []
     for contour in contours:
