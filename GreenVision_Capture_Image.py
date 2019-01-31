8a8e8b59fcff4e72b00e68059b8b588a19670143
ethansky <fragglekids@gmail.com>
2019-01-31 14:57:17 -0600

    Fix distance input
    Former-commit-id: 4c496495e7c97f611e004e4695cebec953f321c7

--- GreenVision_Capture_Image.py
+++ GreenVision_Capture_Image.py
@@ -11,18 +11,11 @@ while True:
     cv2.imshow("test", frame)
     if not ret:
         break
-    k = cv2.waitKey(1) & 0xFF
 
-    # if k % 256 == 27:
-    if k == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-    # elif k % 256 == 32:
-    elif k == 32:
-        # SPACE pressed
-        img_name = 'opencv_image_{}in_'.format(distance)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
+    img_name = 'opencv_image_{}in_'.format(distance)
+    cv2.imwrite(img_name, frame)
+    print("{} written!".format(img_name))
+
+
 cap.release()
 cv2.destroyAllWindows()
