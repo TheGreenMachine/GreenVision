1491adb593113b73e1f56f968f17f809f1edd6f8
ethansky <fragglekids@gmail.com>
2019-01-31 14:54:12 -0600

    Fix key

--- GreenVision_Capture_Image.py
+++ GreenVision_Capture_Image.py
@@ -13,11 +13,13 @@ while True:
         break
     k = cv2.waitKey(1) & 0xFF
 
-    if k % 256 == 27:
+    # if k % 256 == 27:
+    if k == 27:
         # ESC pressed
         print("Escape hit, closing...")
         break
-    elif k % 256 == 32:
+    # elif k % 256 == 32:
+    elif k == 32:
         # SPACE pressed
         img_name = 'opencv_image_{}in_'.format(distance)
         cv2.imwrite(img_name, frame)
