ac97e52457965d3d0826ca77abcb59637555f1d3
ethansky <fragglekids@gmail.com>
2019-01-31 15:02:34 -0600

    Fix thing
    Former-commit-id: 037351eb83c553d1c5b31aa2513442de997618da

--- GreenVision_Capture_Image.py
+++ GreenVision_Capture_Image.py
@@ -4,15 +4,18 @@ cap = cv2.VideoCapture(0)
 cv2.namedWindow('image cap')
 
 while True:
-    distance = int(input('Enter distance in inches: '))
     ret, frame = cap.read()
     cv2.imshow("test", frame)
     if not ret:
         break
 
-    img_name = 'opencv_image_{}in_'.format(distance)
-    cv2.imwrite(img_name, frame)
-    print("{} written!".format(img_name))
+    if cv2.waitKey(1) & 0xFF == ord('c'):
+        distance = int(input('Enter distance in inches: '))
+        img_name = 'opencv_image_{}in_'.format(distance)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+    elif cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
