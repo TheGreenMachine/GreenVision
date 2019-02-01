0f806f729d6611e326e05e805ce82d9398edf591
ethansky <fragglekids@gmail.com>
2019-01-31 18:52:46 -0600

    Fix path

--- GreenVision_Capture_Image.py
+++ GreenVision_Capture_Image.py
@@ -1,6 +1,7 @@
 import cv2
 import json
 import os
+
 with open('values.json') as json_file:
     data = json.load(json_file)
 
@@ -16,7 +17,7 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('c'):
         distance = int(input('Enter distance in inches: '))
         path = '/home/pi/Desktop/GreenVision/Test_Images'
-	img_name = os.path.join(path,'opencv_image_{}in.jpg'.format(distance))
+        img_name = os.path.join(path, 'opencv_image_{}in.jpg'.format(distance))
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
     if cv2.waitKey(1) & 0xFF == ord('q'):
