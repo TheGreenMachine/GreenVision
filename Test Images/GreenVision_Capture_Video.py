285fc5343368fec9e3799598e13f3b19df1ed219
Luke Langefels <luke.langefels@comcast.net>
2019-01-31 18:13:22 -0600

    Move capture scripts to image folder

---
+++ Test Images/GreenVision_Capture_Video.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+import json
+
+with open('values.json') as json_file:
+    data = json.load(json_file)
+
+cap = cv2.VideoCapture(0)
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+output_name = input('Enter name for file: ')
+out = cv2.VideoWriter(output_name + '.avi', fourcc, 30.0, (data['image-width'], data['image-height']))
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+        out.write(frame)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
