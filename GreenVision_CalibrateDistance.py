810907e4ea7fbc1a5007a7e9d10e803986a315b0
Ian <ian.mcvann@gmail.com>
2019-02-02 08:46:55 -0600

    Add distance and camera calibration
    Former-commit-id: 15ff960ce85ce11feda609a76d0651e440fc40d1

--- GreenVision_CalibrateDistance.py
+++ GreenVision_CalibrateDistance.py
@@ -1,10 +1,8 @@
 import cv2
 import numpy as np
-import matplotlib
+from statistics import mean
 import json
-import argparse
-import time
-import glob
+import os
 
 with open('values.json') as json_file:
     data = json.load(json_file)
@@ -12,8 +10,36 @@ with open('values.json') as json_file:
 lower_color = np.array(data['lower-color-list'])
 upper_color = np.array([data['upper-color-list'][0], 255, 255])
 count = 6
-while (1):
-    img = cv2.imread("C:/Users/Nightmaze/Documents/Github/FRC/GreenVision/Test_Images/opencv_image_{}in.jpg".format(count))
+
+xs = np.array([], dtype=np.float64)
+ys = np.array([], dtype=np.float64)
+
+
+def best_fit_slope_and_intercept(xs, ys):
+    m = (((mean(xs) * mean(ys)) - mean(xs * ys)) /
+         ((mean(xs) * mean(xs)) - mean(xs * xs)))
+
+    b = mean(ys) - m * mean(xs)
+
+    return m, b
+def capture(count):
+    path = '/home/pi/Desktop/GreenVision/Test_Images'
+    img_name = os.path.join(path, 'opencv_image_{}in.jpg'.format(count))
+    cv2.imwrite(img_name, frame)
+    print("{} written!".format(img_name))
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, data['image-width'])
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, data['image-height'])
+while (count < 33):
+    print('Line up camera at {}, and press C to capture'.format(count))
+    ret, frame = cap.read()
+    cv2.imshow("test", frame)
+    if cv2.waitKey(1) & 0xFF == ord('c'):
+        capture(count)
+        count +=3
+count = 6
+while (count < 33):
+    img = cv2.imread("/home/pi/Desktop/GreenVision/Test_Images/opencv_image_{}in.jpg".format(count))
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_color, upper_color)
     print("{} in shot".format(count))
@@ -22,9 +48,14 @@ while (1):
     for contour in contours:
         if cv2.contourArea(contour) > 75:
             ncontours.append(contour)
-            contourarea = cv2.contourArea(contour)
-            print("Contour Area: {}".format(contourarea))
-    print("Number of contours: ", len(ncontours))
+    if len(ncontours) <= 2 and len(ncontours) != 0 and len(ncontours) != 1 and len(ncontours) != 3:
+        contourarea = cv2.contourArea(ncontours[0])
+        ys = np.append(ys, contourarea)
+        xs = np.append(xs, count)
+        print(ys)
+        print(xs)
     count +=3
 
+m, b = best_fit_slope_and_intercept(xs, ys)
 
+print("M: {} B: {}".format(m,b))
