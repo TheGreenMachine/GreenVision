98162882ff56f2c77818987e1e4f1a582257926a
ethansky <fragglekids@gmail.com>
2019-02-02 12:09:16 -0600

    Something changed but I forgot what I changed
    Former-commit-id: 261aa084a5fffd27e8d19752c07043d8bbb127d7

--- GreenVision.py
+++ GreenVision.py
@@ -156,8 +156,8 @@ def vision():
     upper_color = np.array([data['upper-color-list'][0] + threshold, 255, 255])
 
     def calc_distance(area):
-        top = area - 10748.25
-        full = top / -323.75
+        top = area - data['b']
+        full = top / data['m']
         return full
 
     def calc_pitch(pixel_y, center_y, v_foc_len):
