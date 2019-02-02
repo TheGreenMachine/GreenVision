e5e68d6f6477244fdf62f34d262539052a8be188
ethansky <fragglekids@gmail.com>
2019-02-02 14:24:05 -0600

    Typo fix
    Former-commit-id: 12a763e53f7d520a2ff8a83120b0fb552e17926a

--- GreenVision_CalibrateDistance.py
+++ GreenVision_CalibrateDistance.py
@@ -57,7 +57,7 @@ while count < 31:
         y_distance = np.append(y_distance, count)
 
     count += 1
-print(curve_fit(lambda t, a, b: a * np.exp(b * t)), x_area, y_distance)
+print(curve_fit(lambda t, a, b: a * np.exp(b * t), x_area, y_distance))
 df = pd.DataFrame({"x": x_area, "y": y_distance, "z": zs})
 # m, b = best_fit_slope_and_intercept(xs, ys)
 df.to_csv("distance_calibrate_dump.csv", index=False)
