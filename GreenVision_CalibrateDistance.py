ae8ebc4c7942f8becbd96d261586792ccf586e40
Ian <ian.mcvann@gmail.com>
2019-02-02 12:22:45 -0600

    Change change ys to distance
    Former-commit-id: 010c8a61ddb8625b5aab5d8237cdf515fb383f1e

--- GreenVision_CalibrateDistance.py
+++ GreenVision_CalibrateDistance.py
@@ -66,9 +66,9 @@ while count < 31:
             ncontours.append(contour)
     if len(ncontours) <= 2 and len(ncontours) != 0 and len(ncontours) != 1 and len(ncontours) != 3:
         contourarea = cv2.contourArea(ncontours[0])
-        xs = np.append(ys, contourarea)
+        xs = np.append(xs, contourarea)
         zs = np.append(zs, math.sqrt(1/(contourarea)))
-        ys = np.append(xs, count)
+        ys = np.append(ys, count)
 
         print(ys)
         print(xs)
