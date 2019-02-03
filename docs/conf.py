a4fdd1bf3b8c08f51b641c9f81781cb914bc728d
Ian <ian.mcvann@gmail.com>
2019-02-03 08:39:01 -0600

    Change theme and add basic text

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'GreenVision'
-copyright = '2019, Ian McVann, Ethan Emmons'
-author = 'Ian McVann, Ethan Emmons'
+copyright = '2019, The Green Machine'
+author = 'The Green Machine'
 
 # The short X.Y version
 version = ''
@@ -76,7 +76,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
