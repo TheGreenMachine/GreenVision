5e4c8bb354c9542852e5869ca4552b65418fdf01
ethansky <fragglekids@gmail.com>
2019-03-02 14:47:02 -0600

    Rewrite vision processing

--- GreenVision.py
+++ GreenVision.py
@@ -198,44 +198,14 @@ def vision():
     upper_color = np.array([data['upper-color-list'][0] + threshold, 255, 255])
 
     class Rect:
-        def __init__(self, rectangle, theta, area):
-            self.tlx = rectangle[0]
-            self.tly = rectangle[1]
-            self.width = rectangle[2]
-            self.height = rectangle[3]
-            self.brx = self.tlx + self.width
-            self.bry = self.tly + self.height
-            self.cx = int((self.tlx + self.brx) / 2)
-            self.cy = int((self.tly + self.bry) / 2)
-            self.angle = theta
-            self.cont_area = area
-
-    def get_rec(rec_l, theta_l, contour_l):
-        big_cont = max(contour_l)
-        big_index = contour_l.index(big_cont)
-        rec = Rect(rec_l[big_index], theta_l[big_index], contour_l[big_index])
-
-        rec_l.pop(big_index)
-        theta_l.pop(big_index)
-        contour_l.pop(big_index)
-        return rec
-
-    def is_pair(ca, cb):
-        if (ca.angle < 0 and cb.angle > 0) or (ca.angle > 0 and cb.angle < 0):
-            print('is_pair() math: {}'.format(ca.angle + cb.angle))
-            are_opposite = np.sign(ca.angle) != np.sign(cb.angle)
-            return are_opposite
-        else:
-            return False
-
-    def calc_angle(con):
-        _, _, theta = cv2.minAreaRect(con)
-        if theta < -50:
-            angle = abs(theta + 90)
-        else:
-            angle = round(theta)
-
-        return angle
+        def __init__(self, contur):
+            center, wid_hei, theta = cv2.minAreaRect(contur)
+            self.center = center
+            self.width = wid_hei[0]
+            self.height = wid_hei[1]
+            self.theta = theta
+            self.box = cv2.boxPoints(cv2.minAreaRect(contur))
+            self.area = cv2.contourArea(contur)
 
     def calc_distance(ca, cb):
         avg_contour = (ca + cb) / 2
@@ -249,18 +219,6 @@ def vision():
     def calc_yaw(pixel_x, center_x, h_foc_len):
         ya = math.degrees(math.atan((pixel_x - center_x) / h_foc_len))
         return round(ya)
-
-    def draw_points(rec_a, rec_b, acx, acy, color):
-        cv2.line(frame, (rec_a.cx, rec_a.cy), (rec_a.cx, rec_a.cy), color, 8)
-        cv2.line(frame, (rec_b.cx, rec_b.cy), (rec_b.cx, rec_b.cy), color, 8)
-        cv2.line(frame, (acx, acy), (acx, acy), (255, 0, 0), 8)
-
-    def get_avg_points(rec_a, rec_b):
-        avgcx = int((rec_a.cx + rec_b.cx) / 2)
-        avgcy = int((rec_a.cy + rec_b.cy) / 2)
-        if debug:
-            print('Average Center (x , y): ({} , {})'.format(avgcx, avgcy))
-        return avgcx, avgcy
 
     def update_net_table(n, c1_x=-1, c1_y=-1, c2_x=-1, c2_y=-1, avgc_x=-1, avgc_y=-1, dis=-1):
         table.putNumber("center{}_x".format(n), c1_x)
@@ -298,79 +256,69 @@ def vision():
         mask = cv2.inRange(hsv, lower_color, upper_color)
 
         screen_c_x = data['image-width'] / 2 - 0.5
-        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        ncontours = []
-
-        contour_area_arr = []
-
-        theta_list = []
-        rec_list = []
-        for contour in contours:
-            if cv2.contourArea(contour) > 50 and cv2.contourArea(contour) < 10000:
-                theta_list.append(calc_angle(contour))
-                contour_area_arr.append(cv2.contourArea(contour))
-                cont_input = contour_area_arr.copy()
-                ncontours.append(contour)
-                rec_list.append(cv2.boundingRect(contour))
-        print("Number of contours: ", len(ncontours))
-        if len(rec_list) == 0:
-            update_net_table(1)
-            continue
-        for contour in ncontours:
-            #cv2.drawContours(frame, [contour], -1, (0, 0, 255), 3)
-            rec_list1 = rec_list.copy()
-            if len(rec_list) > 1:
-
-                print('Angles: {}'.format(theta_list))
-
-                rec1 = get_rec(rec_list, theta_list, cont_input)
-                rec2 = get_rec(rec_list, theta_list, cont_input)
-                print(contour_area_arr)
-                print('Is pair: {}'.format(is_pair(rec1, rec2)))
-                avg_c1_x, avg_c1_y = get_avg_points(rec1, rec2)
-                if is_pair(rec1, rec2):
-                    #draw_points(rec1, rec2, avg_c1_x, avg_c1_y, (255, 0, 0))  # (255, 0, 0) == blue (bgr)
-                    distance = calc_distance(rec1.cont_area, rec2.cont_area)
-                    yaw = calc_yaw(avg_c1_x, screen_c_x, h_focal_length)
-                    if debug:
-                        print('Distance = {} \t Yaw = {} \t is_pair() = {}'.format(distance, yaw, is_pair(rec1, rec2)))
-                    if net_table:
-                        update_net_table(1, rec1.cx, rec1.cy, rec2.cx, rec2.cy, avg_c1_x, avg_c1_y, distance)
-                if len(rec_list) > 3:
-                    rec3 = get_rec(rec_list, theta_list, cont_input)
-                    rec4 = get_rec(rec_list, theta_list, cont_input)
-                    avg_c2_x, avg_c2_y = get_avg_points(rec3, rec4)
-                    yaw1 = calc_yaw(avg_c2_x, screen_c_x, h_focal_length)
-                    if is_pair(rec3, rec4):
-                        #draw_points(rec3, rec4, avg_c2_x, avg_c2_y, (0, 204, 204))  # (0, 204, 204) == yellow (bgr)
-                        if net_table:
-                            update_net_table(2, rec3.cx, rec3.cy, rec4.cx, rec4.cy, avg_c2_x, avg_c2_y)
-
-                    if len(rec_list) > 5:
-                        rec5 = get_rec(rec_list, theta_list, cont_input)
-                        rec6 = get_rec(rec_list, theta_list, cont_input)
-                        avg_c3_x, avg_c3_y = get_avg_points(rec5, rec6)
-                        yaw2 = calc_yaw(avg_c3_x, screen_c_x, h_focal_length)
-                        if is_pair(rec5, rec6):
-                            if net_table:
-                                update_net_table(3, rec5.cx, rec5.cy, rec6.cx, rec6.cy, avg_c3_x,
-                                                 avg_c3_y)
-                            #draw_points(rec5, rec6, avg_c3_x, avg_c3_y, (255, 0, 255))  # (255, 0, 255) == fuschia
-                yaw_list = [math.fabs(yaw), math.fabs(yaw1), math.fabs(yaw2)]
-                if yaw_list[0] != yaw_list[1] or yaw_list[1] != yaw_list[2]:
-                    yaw_selection = min(yaw_list)
-                else:
-                    continue
-                if yaw_selection == yaw_list[0] and is_pair(rec1, rec2) and len(rec_list1) > 1:
-                    draw_points(rec1, rec2, avg_c1_x, avg_c1_y, (255, 0, 0))  # (255, 0, 0) == blue (bgr)
-                elif yaw_selection == yaw_list[1] and is_pair(rec3, rec4) and len(rec_list1) > 3:
-                    draw_points(rec3, rec4, avg_c2_x, avg_c2_y, (0, 204, 204))  # (0, 204, 204) == yellow (bgr)
-                elif yaw_selection == yaw_list[2] and is_pair(rec5, rec6) and len(rec_list1) > 5:
-                    draw_points(rec5, rec6, avg_c3_x, avg_c3_y, (255, 0, 255))  # (255, 0, 255) == fuschia
+        all_contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        filtered_contours = []
+        for contour in all_contours:
+            if 50 < cv2.contourArea(contour) < 10000:
+                filtered_contours.append(contour)
+        print("Number of contours: ", len(filtered_contours))
+        for contour in filtered_contours:
+            cv2.drawContours(frame, [contour], -1, (0, 0, 255), 3)
+        # rec_list1 = rec_list.copy()
+        # if len(rec_list) > 1:
+        #
+        #     print('Angles: {}'.format(theta_list))
+        #
+        #     rec1 = get_rec(rec_list, theta_list, cont_input)
+        #     rec2 = get_rec(rec_list, theta_list, cont_input)
+        #     print(contour_area_arr)
+        #     print('Is pair: {}'.format(is_pair(rec1, rec2)))
+        #     avg_c1_x, avg_c1_y = get_avg_points(rec1, rec2)
+        #     if is_pair(rec1, rec2):
+        #         # draw_points(rec1, rec2, avg_c1_x, avg_c1_y, (255, 0, 0))  # (255, 0, 0) == blue (bgr)
+        #         distance = calc_distance(rec1.cont_area, rec2.cont_area)
+        #         yaw = calc_yaw(avg_c1_x, screen_c_x, h_focal_length)
+        #         if debug:
+        #             print('Distance = {} \t Yaw = {} \t is_pair() = {}'.format(distance, yaw, is_pair(rec1, rec2)))
+        #         if net_table:
+        #             update_net_table(1, rec1.cx, rec1.cy, rec2.cx, rec2.cy, avg_c1_x, avg_c1_y, distance)
+        #     if len(rec_list) > 3:
+        #         rec3 = get_rec(rec_list, theta_list, cont_input)
+        #         rec4 = get_rec(rec_list, theta_list, cont_input)
+        #         avg_c2_x, avg_c2_y = get_avg_points(rec3, rec4)
+        #         yaw1 = calc_yaw(avg_c2_x, screen_c_x, h_focal_length)
+        #         if is_pair(rec3, rec4):
+        #             # draw_points(rec3, rec4, avg_c2_x, avg_c2_y, (0, 204, 204))  # (0, 204, 204) == yellow (bgr)
+        #             if net_table:
+        #                 update_net_table(2, rec3.cx, rec3.cy, rec4.cx, rec4.cy, avg_c2_x, avg_c2_y)
+        #
+        #         if len(rec_list) > 5:
+        #             rec5 = get_rec(rec_list, theta_list, cont_input)
+        #             rec6 = get_rec(rec_list, theta_list, cont_input)
+        #             avg_c3_x, avg_c3_y = get_avg_points(rec5, rec6)
+        #             yaw2 = calc_yaw(avg_c3_x, screen_c_x, h_focal_length)
+        #             if is_pair(rec5, rec6):
+        #                 if net_table:
+        #                     update_net_table(3, rec5.cx, rec5.cy, rec6.cx, rec6.cy, avg_c3_x,
+        #                                      avg_c3_y)
+        #                 # draw_points(rec5, rec6, avg_c3_x, avg_c3_y, (255, 0, 255))  # (255, 0, 255) == fuschia
+        #     yaw_list = [math.fabs(yaw), math.fabs(yaw1), math.fabs(yaw2)]
+        #     if yaw_list[0] != yaw_list[1] or yaw_list[1] != yaw_list[2]:
+        #         yaw_selection = min(yaw_list)
+        #     else:
+        #         continue
+        #     if yaw_selection == yaw_list[0] and is_pair(rec1, rec2) and len(rec_list1) > 1:
+        #         draw_points(rec1, rec2, avg_c1_x, avg_c1_y, (255, 0, 0))  # (255, 0, 0) == blue (bgr)
+        #     elif yaw_selection == yaw_list[1] and is_pair(rec3, rec4) and len(rec_list1) > 3:
+        #         draw_points(rec3, rec4, avg_c2_x, avg_c2_y, (0, 204, 204))  # (0, 204, 204) == yellow (bgr)
+        #     elif yaw_selection == yaw_list[2] and is_pair(rec5, rec6) and len(rec_list1) > 5:
+        #         draw_points(rec5, rec6, avg_c3_x, avg_c3_y, (255, 0, 255))  # (255, 0, 255) == fuschia
 
         if view:
-            cv2.putText(frame, 'Yaws: {} {} {}'.format(yaw, yaw1, yaw2), (10, 500), cv2.FONT_HERSHEY_COMPLEX, .5, (255, 255, 255), 2)
-            cv2.putText(frame, 'Contour areas: {}'.format(contour_area_arr), (10, 600), cv2.FONT_HERSHEY_COMPLEX, .5, (255, 255, 255), 2)
+            # cv2.putText(frame, 'Yaws: {} {} {}'.format(yaw, yaw1, yaw2), (10, 500), cv2.FONT_HERSHEY_COMPLEX, .5,
+            #             (255, 255, 255), 2)
+            # cv2.putText(frame, 'Contour areas: {}'.format(contour_area_arr), (10, 600), cv2.FONT_HERSHEY_COMPLEX, .5,
+            #             (255, 255, 255), 2)
             cv2.imshow('Contour Window', frame)
             cv2.imshow('Mask', mask)
         if cv2.waitKey(1) & 0xFF == ord('q'):
