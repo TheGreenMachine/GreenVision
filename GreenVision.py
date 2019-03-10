2406838600e2dd059c0a3e88f843c2e02f098970
ethansky <fragglekids@gmail.com>
2019-03-09 23:56:02 -0600

    Add todo list useful links, and some prelim solvePnP code and camera calibration
    Former-commit-id: 4530fb9ce5b56d0d9657fe910ce6088d1e057067

--- GreenVision.py
+++ GreenVision.py
@@ -1,16 +1,16 @@
-import numpy as np
-import cv2
-import networktables as nt
-from imutils.video import WebcamVideoStream
+import argparse
+import bisect
 import json
 import math
-import time
-import argparse
 import os
-import pandas as pd
+import time
+
+import cv2
 import imutils
-import bisect
-import threading
+import networktables as nt
+import numpy as np
+import pandas as pd
+from imutils.video import WebcamVideoStream
 
 with open('/home/pi/GreenVision/values.json') as json_file:
     data = json.load(json_file)
@@ -120,6 +120,23 @@ def init_parser_video():
                               help='choose a different name for the file')
 
 
+def init_camera_calibration():
+    parser_calibration = subparsers.add_parser('camera_calibration')
+    parser_calibration.add_argument_group('Camera Calibration Arguments')
+    parser_calibration.add_argument('--length', '-l',
+                                    type=int,
+                                    default=9,
+                                    help='length of checkerboard (number of corners)')
+    parser_calibration.add_argument('--width', '-w',
+                                    type=int,
+                                    default=6,
+                                    help='width of checkerboard (number of corners)')
+    parser_calibration.add_argument('--size', '-s',
+                                    type=float,
+                                    default=1.0,
+                                    help='size of square')
+
+
 def init_parser_distance_table():
     parser_distance_table = subparsers.add_parser('distance_table')
     parser_distance_table.add_argument_group('Distance table Arguments')
@@ -227,7 +244,6 @@ def vision():
 
     if net_table:
         nt.NetworkTables.initialize(server=data['server-ip'])
-        nt.NetworkTables.addConnectionListener(connection_listener, immediateNotify=True)
         table = nt.NetworkTables.getTable('SmartDashboard')
         if table:
             print('table OK')
@@ -403,6 +419,97 @@ def video_capture():
     cap.release()
     out.release()
     cv2.destroyAllWindows()
+
+
+def camera_calibration():
+    class CameraCalibration(object):
+        """Calibrates a distorted camera courtesy of Ligerbots"""
+
+        def __init__(self):
+            # number of inside corners in the chessboard (height and width)
+            self.checkerboard_height = 9
+            self.checkerboard_width = 6
+
+            # size of chessboard square in physical units
+            self.square_size = 1.0
+
+            self.shape = None
+            return
+
+        def calibrate_camera(self):
+            cwd = os.path.join(os.getcwd(), 'Camera_Calibration')  # /home/pi/GreenVision/Camera_Calibration
+            if not os.path.exists(cwd):
+                os.makedirs(cwd)
+            # Calculates the distortion co-efficients
+            # termination criteria
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+            # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+            objp = np.zeros((self.checkerboard_height * self.checkerboard_width, 3), np.float32)
+            objp[:, :2] = np.mgrid[0:self.checkerboard_width, 0:self.checkerboard_height].T.reshape(-1, 2)
+            # calibrate coordinates to the physical size of the square
+            objp *= self.square_size
+            # Arrays to store object points and image points from all the images.
+            objpoints = []  # 3d point in real world space
+            imgpoints = []  # 2d points in image plane.
+
+            for fname in cwd:
+                print('Processing file', fname)
+                img = cv2.imread(fname)
+
+                if img is None:
+                    print("ERROR: Unable to read file", fname)
+                    continue
+                self.shape = img.shape
+
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+                # Find the chess board corners
+                ret, corners = cv2.findChessboardCorners(gray, (self.checkerboard_width, self.checkerboard_height),
+                                                         None)
+
+                # If found, add object points, image points (after refining them)
+                if ret:
+                    objpoints.append(objp)
+                    corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+                    imgpoints.append(corners2)
+                    # Draw and display the corners
+                    img = cv2.drawChessboardCorners(img, (self.checkerboard_width, self.checkerboard_height), corners2,
+                                                    ret)
+                    cv2.imshow('img', img)
+                    cv2.waitKey(500)
+                else:
+                    print(fname, 'failed')
+
+            cv2.destroyAllWindows()
+
+            if not objpoints:
+                print("No useful images. Quitting...")
+                return None
+
+            print('Found {} useful images'.format(len(objpoints)))
+            ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+            return ret, mtx.tolist(), dist.tolist(), rvecs, tvecs
+
+    calibrate = CameraCalibration()
+    calibrate.checkerboard_width = args['width']
+    calibrate.checkerboard_height = args['height']
+    calibrate.square_size = args['size']
+
+    ret, mtx, dist, rvecs, tvecs = calibrate.calibrate_camera()
+    print('reprojection error =', ret)
+    print('image center = ({:.2f}, {:.2f})'.format(mtx[0][2], mtx[1][2]))
+
+    fov_x = math.degrees(2.0 * math.atan(calibrate.shape[1] / 2.0 / mtx[0][0]))
+    fov_y = math.degrees(2.0 * math.atan(calibrate.shape[0] / 2.0 / mtx[1][1]))
+    print('FOV = ({:.2f}, {:.2f}) degrees'.format(fov_x, fov_y))
+
+    print('mtx =', mtx)
+    print('dist =', dist)
+
+    # Writing JSON data
+    with open('output.json', 'w') as f:
+        json.dump({"camera_matrix": mtx, "distortion": dist}, f)
 
 
 def distance_table():
