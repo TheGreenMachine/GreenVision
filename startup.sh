f2a958f03203f1de4d40f605aa3c94bfd69a5d74
ethansky <fragglekids@gmail.com>
2019-03-19 20:50:42 -0500

    Clean up raspberry pi files and updated the repo version of the startup.sh
    Former-commit-id: df1f66549bfe6cf2424b441182468aeae5685980

--- startup.sh
+++ startup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-
 source /home/pi/prof
 workon cv
-v4l2-ctl --set-ctrl exposure_auto=1
-v4l2-ctl --set-ctrl exposure_absolute=4
-v4l2-ctl --set-ctrl brightness=1
-
-python3 /home/pi/GreenVision/GreenVision.py vision -d -mt -nt -f -th 30 -s 0 -f
+#v4l2-ctl --set-ctrl exposure_auto=1
+#v4l2-ctl --set-ctrl exposure_absolute=10
+#v4l2-ctl --set-ctrl brightness=1
+#python3 /home/pi/seecam.py
+sleep 3
+cd GreenVision
+python3 seecam_comp.py
+#python3 /home/pi/GreenVision/GreenVision.py vision -d -mt -nt -f -th 30 -s 0
+python3 GreenVision.py vision -d -nt -th 15 -s 0
