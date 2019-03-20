7fb7a17aec6a842cbcf3ed479b34539c1b641f58
ethansky <fragglekids@gmail.com>
2019-03-19 20:16:03 -0500

    Update comp cam with working values
    Former-commit-id: afd13cccaf7f0d5daf4da2cf85dfc665ba63daf3

--- seecam_comp.py
+++ seecam_comp.py
@@ -10,8 +10,8 @@ control.get_control_value(9963776)
 # get all default settings
 settings = control.get_controls()
 
-# for setting in settings:
-#     print("Value {} = {}".format(setting['name'], setting['value']))
+for setting in settings:
+    print("Value {} = {}".format(setting['name'], setting['value']))
 
 # set desired values
 print()
@@ -27,13 +27,13 @@ control.set_control_value(9963802, 3250)  # white_balance_temperature
 control.close()
 print("------    DONE    ----")
 
-# COMP CAM
-#                     brightness (int)    : min=-64 max=64 step=1 default=-8193 value=1
-#                       contrast (int)    : min=0 max=64 step=1 default=57343 value=8
-#                     saturation (int)    : min=0 max=128 step=1 default=57343 value=8
+# COMP CAM (as of 3/19/19)
+#                     brightness (int)    : min=-64 max=64 step=1 default=-8193 value=-64
+#                       contrast (int)    : min=0 max=64 step=1 default=57343 value=32
+#                     saturation (int)    : min=0 max=128 step=1 default=57343 value=64
 #                            hue (int)    : min=-40 max=40 step=1 default=-8193 value=0
 # white_balance_temperature_auto (bool)   : default=1 value=1
-#                          gamma (int)    : min=72 max=500 step=1 default=57343 value=72
+#                          gamma (int)    : min=72 max=500 step=1 default=57343 value=200
 #                           gain (int)    : min=0 max=100 step=1 default=57343 value=0
 #           power_line_frequency (menu)   : min=0 max=2 default=1 value=2
 #                0: Disabled
@@ -45,10 +45,10 @@ print("------    DONE    ----")
 #                  exposure_auto (menu)   : min=0 max=3 default=0 value=1
 #                1: Manual Mode
 #                3: Aperture Priority Mode
-#              exposure_absolute (int)    : min=1 max=5000 step=1 default=157 value=4
+#              exposure_absolute (int)    : min=1 max=5000 step=1 default=157 value=20
 #         exposure_auto_priority (bool)   : default=0 value=1
 
-
+# ----------------------------------------------------------------------------------------------------------------- #
 #                     brightness (int)    : min=0 max=15 step=1 default=-8193 value=1
 #                       contrast (int)    : min=0 max=15 step=1 default=57343 value=15
 #                     saturation (int)    : min=0 max=15 step=1 default=57343 value=15
