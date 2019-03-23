c66b83c3fefbdee829c8415f8582e430c869a790
ethansky <fragglekids@gmail.com>
2019-03-23 16:10:57 -0500

    Update seecam values, rewrite seenet to do logging and print all debug info from robot

--- seenet.py
+++ seenet.py
@@ -1,53 +1,67 @@
 import networktables as nt
 import time
 import sys
+import csv
 
 values = {}
 values['visionX'] = -99
 values['visionY'] = -99
 values['width'] = -99
 values['height'] = -99
-values['center0_x'] = -99
-values['center0_y'] = -99
-values['center1_x'] = -99
-values['center1_y'] = -99
 values['center_x'] = -99
 values['center_y'] = -99
 values['distance_esti'] = -99
+values['contours'] = -99
+values['targets'] = -99
+values['yaw'] = -99
+values['pitch'] = -99
 
 
 def view():
-    #global values
-    msg = "\rvis: {:3.1f} {:3.1f}; wid,ht: {:3.1f} {:3.1f}; cent0: {:3.1f} {:3.1f}; cent1: {:3.1f} {:3.1f}, cent: {:3.1f} {:3.1f}; dist: {:3.1f}            ".format( values['visionX'], values['visionY'], values['width'], values['height'], values['center0_x'], values['center0_y'], values['center1_x'], values['center1_y'], values['center_x'], values['center_y'], values['distance_esti'])
-    sys.stdout.write(msg)
+    # global values
+    msg = "\rw, h: ({} {});" \
+          " coords: ({} {});" \
+          " yaw, pitch: ({}, {});" \
+          " dist: {};" \
+          " contours: {};" \
+          " targets: {}".format(
+        int(values['width']), int(values['height']),
+        int(values['center_x']), int(values['center_y']),
+        values['yaw'], values['pitch'],
+        values['distance_esti'],
+        int(values['contours']),
+        int(values['targets']))
+    with open('vision_net_values.csv', mode='a+') as vnv_file:
+        vnv_writer = csv.writer(vnv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        vnv_writer.writerow([int(values['center_x']), int(values['center_y']), int(values['contours']),
+                             int(values['targets'])])
+        sys.stdout.write(msg)
 
+    def value_changed(table, key, value, isNew):
+        global values
+        values[key] = value
+        if key == 'contours':
+            view()
 
-def value_changed(table, key, value, isNew):
-    global values
-    values[key] = value
-    if key == 'distance_esti':
-        view()
+    def start_listener():
+        # During vision itit
+        table.addEntryListener(value_changed, key="width")
+        table.addEntryListener(value_changed, key="height")
+        # Updated during match
+        table.addEntryListener(value_changed, key='center_x')
+        table.addEntryListener(value_changed, key='center_y')
+        table.addEntryListener(value_changed, key='yaw')
+        table.addEntryListener(value_changed, key='distance_esti')
+        table.addEntryListener(value_changed, key='contours')
+        table.addEntryListener(value_changed, key='targets')
+        table.addEntryListener(value_changed, key='pitch')
 
-def start_listener():
-    table.addEntryListener(value_changed, key="visionX")
-    table.addEntryListener(value_changed, key="visionY")
-    table.addEntryListener(value_changed, key="width")
-    table.addEntryListener(value_changed, key="height")
+    nt.NetworkTables.initialize(server='10.18.16.2')
+    table = nt.NetworkTables.getTable('SmartDashboard')
 
-    table.addEntryListener(value_changed, key="center0_x")
-    table.addEntryListener(value_changed, key="center0_y")
-    table.addEntryListener(value_changed, key="center1_x")
-    table.addEntryListener(value_changed, key="center1_y")
-    table.addEntryListener(value_changed, key="center_x")
-    table.addEntryListener(value_changed, key="center_y")
-    table.addEntryListener(value_changed, key="distance_esti")
+    view()
 
-nt.NetworkTables.initialize(server='10.18.16.2')
-table = nt.NetworkTables.getTable('SmartDashboard')
+    start_listener()
 
-view()
-
-start_listener()
-
-while True:
-    time.sleep(1)
+    while True:
+        time.sleep(1)
