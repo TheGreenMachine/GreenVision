5316cd5fc02bc0602bd7d9d9726661fa346311ef
ethansky <fragglekids@gmail.com>
2019-03-23 16:30:38 -0500

    Fix indent issues in seecam
    Former-commit-id: a6f10d089174b9809b31c6a0888348c1baed1d2e

--- seenet.py
+++ seenet.py
@@ -35,33 +35,36 @@ def view():
         vnv_writer = csv.writer(vnv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         vnv_writer.writerow([int(values['center_x']), int(values['center_y']), int(values['contours']),
                              int(values['targets'])])
-        sys.stdout.write(msg)
+    sys.stdout.write(msg)
 
-    def value_changed(table, key, value, isNew):
-        global values
-        values[key] = value
-        if key == 'contours':
-            view()
 
-    def start_listener():
-        # During vision itit
-        table.addEntryListener(value_changed, key="width")
-        table.addEntryListener(value_changed, key="height")
-        # Updated during match
-        table.addEntryListener(value_changed, key='center_x')
-        table.addEntryListener(value_changed, key='center_y')
-        table.addEntryListener(value_changed, key='yaw')
-        table.addEntryListener(value_changed, key='distance_esti')
-        table.addEntryListener(value_changed, key='contours')
-        table.addEntryListener(value_changed, key='targets')
-        table.addEntryListener(value_changed, key='pitch')
+def value_changed(table, key, value, isNew):
+    global values
+    values[key] = value
+    if key == 'contours':
+        view()
 
-    nt.NetworkTables.initialize(server='10.18.16.2')
-    table = nt.NetworkTables.getTable('SmartDashboard')
 
-    view()
+def start_listener():
+    # During vision itit
+    table.addEntryListener(value_changed, key="width")
+    table.addEntryListener(value_changed, key="height")
+    # Updated during match
+    table.addEntryListener(value_changed, key='center_x')
+    table.addEntryListener(value_changed, key='center_y')
+    table.addEntryListener(value_changed, key='yaw')
+    table.addEntryListener(value_changed, key='distance_esti')
+    table.addEntryListener(value_changed, key='contours')
+    table.addEntryListener(value_changed, key='targets')
+    table.addEntryListener(value_changed, key='pitch')
 
-    start_listener()
 
-    while True:
-        time.sleep(1)
+nt.NetworkTables.initialize(server='10.18.16.2')
+table = nt.NetworkTables.getTable('SmartDashboard')
+
+view()
+
+start_listener()
+
+while True:
+    time.sleep(1)
