a01b36a900d4f6a3515f3a033ebd8c1f8c01e685
ethansky <fragglekids@gmail.com>
2019-04-06 01:14:34 -0500

    Transition to using (x,y) tuples, add sudo mount
    Former-commit-id: 10a67eeaaf2543def71af99b4feb0ddc6b41bf05

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ sleep 3
 cd GreenVision
 python3 seecam_comp.py
 if [ -e /dev/sda1 ];
-then mount /dev/sda1 /media/pi/GVLOGGING;
+then sudo mount /dev/sda1 /media/pi/GVLOGGING;
 python3 GreeVision.py -s 0 -f -nt --pi -l;
 else
 python3 GreenVision.py -s 0 -d -nt --pi;
