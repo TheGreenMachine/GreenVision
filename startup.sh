f18e77ec70843c0bce5f762f72f634ff8af09e5d
ethansky <fragglekids@gmail.com>
2019-04-05 23:24:35 -0500

    add mounting on usb if usb is present in /dev/
    Former-commit-id: 8291dd1f24a0fab44acc857112d8a7b64be58c43

--- startup.sh
+++ startup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
 source /home/pi/prof
 workon cv
-#v4l2-ctl --set-ctrl exposure_auto=1
-#v4l2-ctl --set-ctrl exposure_absolute=10
-#v4l2-ctl --set-ctrl brightness=1
-#python3 /home/pi/seecam.py
 sleep 3
 cd GreenVision
 python3 seecam_comp.py
-#python3 /home/pi/GreenVision/GreenVision.py vision -d -mt -nt -f -th 30 -s 0
-python3 GreenVision.py vision -d -nt -th 15 -s 0
+if [ -e /dev/sda1 ];
+then mount /dev/sda1 /media/pi/GVLOGGING;
+python3 GreeVision.py -s 0 -f -nt --pi -l;
+else
+python3 GreenVision.py -s 0 -d -nt --pi;
+fi
