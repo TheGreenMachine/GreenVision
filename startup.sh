06b68978a405226481b4a656058641444202a781
ethansky <fragglekids@gmail.com>
2019-04-18 18:42:55 -0500

    Update values
    Former-commit-id: c0b4985df96d3d10ba165aaa4f7f6a48d8af1d7d

--- startup.sh
+++ startup.sh
@@ -10,8 +10,8 @@ if [ -e /dev/sda1 ]
 then sudo mount -o rw,users,umask=000 /dev/sda1 /media/pi/GVLOGGING
 	echo "Mounting logging USB..."
 	sleep 1
-	python3 GreenVision_NSR.py -s 0 -d -nt --pi -l
+	python3 GreenVision_NSR.py -s 0 -d -nt --pi -l -ath 7 -th 5
 else
 	echo "Failed to mount USB!"
-	python3 GreenVision_NSR.py -s 0 -d -nt --pi
+	python3 GreenVision_NSR.py -s 0 -d -nt --pi -ath 7 -th 5
 fi
